fc883a36ed9fa8a8618197be521a3b14c35bd747
Matteo Menapace <hello@baddeo.com>
2016-05-29 23:22:18 +0100

    config

---
+++ RENAME_ME_config.js
@@ -0,0 +1,15 @@
+module.exports = 
+{
+	twitterKeys:
+	{
+		consumer_key: 'YOUR_TWITTER_APP_CONSUMER_KEY',
+		consumer_secret: 'YOUR_TWITTER_APP_CONSUMER_SECRET',
+		access_token_key: 'YOUR_TWITTER_APP_TOKEN_KEY',
+		access_token_secret: 'YOUR_TWITTER_APP_TOKEN_SECRET'
+	},
+	yandexKey: 'YOUR_YANDEX_TRANSLATE_API_KEY',
+	zoom: 3, // the zoom level for Google StreetView (1-5)
+  x: 3, // with zoom=3 there are 6 columns, x=3 gives you the "back of the Google Car" view
+  y: 1, // with zoom=3 there are 3 rows, y=1 gives you a good balance (not too much sky, not too much road)
+	testMode: true // if true, the bot will generate images and , but WON'T tweet anything
+}
