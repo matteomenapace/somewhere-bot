fbeff7fd1274d7826bd23b0ff2d282d8d6df4462
Aimee Matthew-John <aimeebethmj@gmail.com>
2016-05-30 19:43:37 +0100

    Cleaning up indentation :/

--- index.js
+++ index.js
@@ -15,59 +15,59 @@ var config = require('./config'),
 
 function getRandomLocation()
 {
-    var location = [ randomLatitude(), randomLongitude() ]
-    return location
+  var location = [ randomLatitude(), randomLongitude() ]
+  return location
 }
 
 // let's define a recursive function 
 // which will call itself until it finds a location with StreetView
 function tryAndGetRandomStreetView(location)
 {
-    getPanoramaByLocation(location, function (err, result) 
+  getPanoramaByLocation(location, function (err, result) 
+  {
+    if (err) 
     {
-        if (err) 
-        {
-            console.error(err)
-            locationAttempts ++ // increment the attempts count
-            // and try again..
-            tryAndGetRandomStreetView(getRandomLocation())
-        }    
-        else 
-        {
-            console.log('It took ' + locationAttempts + ' attempts to found one!')
-            // console.log(result)
-            getPanoramaInfo(result)
-            processPanoramaImage(result)
-            // getPanoramaImages(result)
-        }
-    })   
+      console.error(err)
+      locationAttempts ++ // increment the attempts count
+      // and try again..
+      tryAndGetRandomStreetView(getRandomLocation())
+    }    
+    else 
+    {
+      console.log('It took ' + locationAttempts + ' attempts to found one!')
+      // console.log(result)
+      getPanoramaInfo(result)
+      processPanoramaImage(result)
+      // getPanoramaImages(result)
+    }
+  })   
 }
 // ...and make it start
 tryAndGetRandomStreetView(getRandomLocation())
 
 function getPanoramaImages(panorama)
 {
-    var id = panorama.id
-        
-    var tiles = getPanoramaTiles(zoom, panorama.tiles)
-    // console.log(tiles)
+  var id = panorama.id
+      
+  var tiles = getPanoramaTiles(zoom, panorama.tiles)
+  // console.log(tiles)
 
-    var images = {}
-    for (var y = 0; y < tiles.rows; y++) 
+  var images = {}
+  for (var y = 0; y < tiles.rows; y++) 
+  {
+    for (var x = 0; x < tiles.columns; x++) 
     {
-        for (var x = 0; x < tiles.columns; x++) 
-        {
-            var key = x + '-' + y
-            images[key] = 
-            {
-                url: getPanoramaURL(id, { x: x, y: y, zoom: zoom }),
-                position: [ x * tiles.tileWidth, y * tiles.tileHeight ]
-            }
-        }
+      var key = x + '-' + y
+      images[key] = 
+      {
+        url: getPanoramaURL(id, { x: x, y: y, zoom: zoom }),
+        position: [ x * tiles.tileWidth, y * tiles.tileHeight ]
+      }
     }
-    
-    console.log(images)
-    return images
+  }
+  
+  console.log(images)
+  return images
 }
 
 function getPanoramaInfo(panorama)
@@ -78,9 +78,14 @@ function getPanoramaInfo(panorama)
     else 
     {
       console.log(result.Location)
-      greetings = 'Greetings from ' + result.Location.region,
-      hashtag = getHashtag(result.Location.country)
+      
+      var place = result.Location.region.split(',')[0]
+          
+      greetings = 'Greetings from ' + place
       console.log(greetings)
+      
+      hashtag = getHashtag(result.Location.country)
+      
       translateIntoCountryLanguage(greetings, result.Location.country)
     }    
   })
@@ -88,30 +93,30 @@ function getPanoramaInfo(panorama)
 
 function translateIntoCountryLanguage(text, countryName)
 {
-    var country = countryLookup.countries({name: countryName})[0]
-    console.log(country)
-    var options = country ? {to: country.languages[0]} : {to: countryName.toLowerCase().substring(0, 3)} 
-    // if the country can't be found, pick the first 3 letters from its name
-    
-    translator.translate(text, options, function(err, res) 
+  var country = countryLookup.countries({name: countryName})[0]
+  console.log(country)
+  var options = country ? {to: country.languages[0]} : {to: countryName.toLowerCase().substring(0, 3)} 
+  // if the country can't be found, pick the first 3 letters from its name
+  
+  translator.translate(text, options, function(err, res) 
+  {
+    if (err) 
     {
-        if (err) 
-        {
-            console.error(err)
-            tweetText = greetings + hashtag
-        }    
-        else
-        {
-           console.log(res)
-           tweetText = res.text[0] + hashtag  
-        }
-        console.log('TWEET > ' + tweetText)
-    })
+      console.error(err)
+      tweetText = greetings + hashtag
+    }    
+    else
+    {
+      console.log(res)
+      tweetText = res.text[0] + hashtag  
+    }
+    console.log('TWEET > ' + tweetText)
+  })
 }
 
 function getHashtag(string)
 {
-    return ' #' + string.replace(/\s/g, '') // no spaces in hashtags
+  return ' #' + string.replace(/\s/g, '') // no spaces in hashtags
 }
 
 function processPanoramaImage(panorama)
@@ -132,11 +137,6 @@ function processPanoramaImage(panorama)
 
       if (filter.brightness) newImage.brightness(filter.brightness)
       if (filter.contrast) newImage.contrast(filter.contrast)
-      
-      // TODO sepia
-
-      // TODO Instagram filters
-      // newImage.filter(filterName)
 
       var colorArray = getColorArray(filter)
       if (colorArray.length > 0) newImage.color(colorArray)
