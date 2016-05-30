ef45082d2817dba30840d9f03b6fccfb88d72960
Aimee Matthew-John <aimeebethmj@gmail.com>
2016-05-30 19:17:23 +0100

    Filters

---
+++ filters.js
@@ -0,0 +1,33 @@
+module.exports =
+{
+  rina:
+  {
+    contrast: 0.23
+  },
+  nadia:
+  {
+    contrast: 0.53,
+    hue: 23,
+  },
+  nostalgia:
+  {
+    brightness: 0.1,
+    contrast: 0.5,
+    saturate: -15,
+  },
+  sunrise:
+  {
+    brightness: 0.1,
+    contrast: 0.5,
+    red: 15
+  },
+  nashville:
+  {
+    brightness: 0.1,
+    contrast: 0.55,
+    red: 33,
+    blue: 10,
+    green: 28,
+    shade: 1
+  }
+}
