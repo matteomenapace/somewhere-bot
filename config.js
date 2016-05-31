c4dccb01d0642ce4a0ca005bcc2d86fe1765fb2d
Matteo Menapace <hello@baddeo.com>
2016-05-31 08:45:06 +0100

    Actually added config...

---
+++ config.js
@@ -0,0 +1,17 @@
+module.exports = 
+{
+	twitterKeys:
+	{
+		consumer_key: 'ZC5yJCBKvkYk47UfSgfpmtBKl',
+		consumer_secret: 'gvpIhegeIaGnMY9r4qlGIMu8oGacF8oZwaQ0jkES0AgRQanoq2',
+		access_token_key: '737343032178167808-g3NXQNENgcGwx4AsF0zPcTDHvmCamiz',
+		access_token_secret: 'z3pmjcKj0qPAhVVzqTal7zb2qoZCF9oJMltlv0CRAgcMW'
+	},
+	yandexKey: 'trnsl.1.1.20160529T204303Z.b2cda660a509c9cb.c93db75d9b04b3a63ece7ad1e5750f4a6c5ce76c',
+	zoom: 3, // the zoom level for Google StreetView (1-5)
+  x: 3, // with zoom=3 there are 6 columns, x=3 gives you the "back of the Google Car" view
+  y: 1, // with zoom=3 there are 3 rows, y=1 gives you a good balance (not too much sky, not too much road)
+
+  translateCaption: false,
+	testMode: false // if true, the bot will generate images and , but WON'T tweet anything
+}
