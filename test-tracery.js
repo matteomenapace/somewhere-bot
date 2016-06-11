88d5497d327838e2be5df59c3034e837eae9fb33
Matteo Menapace <hello@baddeo.com>
2016-06-11 14:15:12 +0100

    CopyWriter.js

--- test-tracery.js
+++ test-tracery.js
@@ -1,8 +1,8 @@
-var text = require('./text')
+var copyWriter = require('./CopyWriter')
 
 for (var i = 20; i >= 0; i--) 
 {
-  var string = text.generate()/*,
+  var string = copyWriter.generate()/*,
       words = string.split(' '),
       lastWord = words[words.length - 1],
       rhymes = pronouncing.rhymes(lastWord)*/
